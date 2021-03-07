14b0ab8839063580d9b260de5af3b01e1870fc94
mizza-cz <mizza.cz666@gmail.com>
2021-03-07 17:03:16 +0100

    add shop-detail page

--- app/js/main.js
+++ app/js/main.js
@@ -9,6 +9,28 @@ menuIcon.addEventListener('click', () => {
   navbar.classList.toggle('change');
 });
 
+const btnMore = document.querySelector('.more');
+const shopDetailBox = document.querySelector('.shop-detail__box');
+btnMore.addEventListener('click', () => {
+  shopDetailBox.classList.toggle('vision');
+});
+
+
+
+// const btnShop = document.querySelector('.shops__btn');
+// const shopItem = document.querySelector('.shops__item');
+// btnShop.addEventListener('click', () => {
+//   shopItem.classList.toggle('active');
+// });
+
+$('.shops__btn').on('click', function () {
+  $(this).closest('shops__item').removeClass('active');
+  $(this).parent().addClass('active');
+
+  return false;
+});
+
+
 jQuery(document).ready(function ($) {
   $('.gallery__inner').magnificPopup({
     delegate: 'a',
