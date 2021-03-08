924112837f3b513b89ae45cc65cfdd20786d6cf8
mizza-cz <mizza.cz666@gmail.com>
2021-03-08 09:18:09 +0100

    +js script

--- app/js/main.js
+++ app/js/main.js
@@ -1,3 +1,27 @@
+jQuery(document).ready(function ($) {
+  $('.shops__item  .btn-arrow').click(function () {
+    $(this).parents('.shops__item').addClass('actives'); //добавляем класс текущей (нажатой)
+  });
+
+  $('.shops__item  .shop-close').click(function () {
+    $(this).parents('.shops__item').removeClass('actives'); //добавляем класс текущей (нажатой)
+  });
+  $('.best__item-content .best__btn .btn-arrow').click(function () {
+    $(this).parents('.best__item-content').addClass('active'); //добавляем класс текущей (нажатой)
+  });
+
+  $('.best__item-content .best__btn .shop-close').click(function () {
+    $(this).parents('.best__item-content').removeClass('active'); //добавляем класс текущей (нажатой)
+  });
+
+  $('.gallery__inner').magnificPopup({
+    delegate: 'a',
+    type: 'image',
+    gallery: {
+      enabled: true,
+    },
+  });
+});
 window.addEventListener('scroll', function () {
   var header = document.querySelector('.header');
   header.classList.toggle('sticky', window.scrollY > 0);
@@ -15,30 +39,9 @@ btnMore.addEventListener('click', () => {
   shopDetailBox.classList.toggle('vision');
 });
 
-// const btnShop = document.querySelector('.shops__btn');
-// const shopItem = document.querySelector('.shops__item');
-// btnShop.addEventListener('click', () => {
-//   shopItem.classList.toggle('active');
-// });
-
-// $('.shops__btn').on('click', function () {
-//   $(this).closest('shops__item').removeClass('active');
-//   $(this).parent().addClass('active');
-
-//   return false;
-// });
-$('.best__btn').on('click', function () {
-  $(this).closest('best__item-content').removeClass('active');
+$('.shops__btn').on('click', function () {
+  $(this).closest('shops__item').removeClass('active');
   $(this).parent().addClass('active');
 
   return false;
-});
-jQuery(document).ready(function ($) {
-  $('.gallery__inner').magnificPopup({
-    delegate: 'a',
-    type: 'image',
-    gallery: {
-      enabled: true,
-    },
-  });
 });
