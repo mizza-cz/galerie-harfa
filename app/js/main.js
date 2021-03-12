fd7361b69351b33096fae57b52dcaabbdbaa5896
mizza-cz <mizza.cz666@gmail.com>
2021-03-12 17:43:39 +0100

    add career-detail

--- app/js/main.js
+++ app/js/main.js
@@ -13,7 +13,13 @@ jQuery(document).ready(function ($) {
   $('.best__item-content .best__btn .shop-close').click(function () {
     $(this).parents('.best__item-content').removeClass('active'); //добавляем класс текущей (нажатой)
   });
+  //video terasy
+  $('.btn__video').on('click', function () {
+    $(this).closest('top__inner').removeClass('top-play');
+    $(this).parent().addClass('top-play');
+  });
 
+  ////
   $('.gallery__inner').magnificPopup({
     delegate: 'a',
     type: 'image',
@@ -21,46 +27,36 @@ jQuery(document).ready(function ($) {
       enabled: true,
     },
   });
-  const btnMore = document.querySelector('.more');
-  const shopDetailBox = document.querySelector('.shop-detail__box');
-  btnMore.addEventListener('click', () => {
-    shopDetailBox.classList.toggle('vision');
+  $('.shops__btn').on('click', function () {
+    $(this).closest('shops__item').removeClass('active');
+    $(this).parent().addClass('active');
+
+    return false;
   });
 });
-var video = document.querySelector('.video');
-var btnVideo = document.getElementById('play');
-var topInner = document.querySelector('.top__inner');
-function togglePlayPause() {
-  if (video.paused) {
-    btnVideo.className = 'pause';
-    video.play();
-  } else {
-    btnVideo.className = 'play';
-    video.pause();
-  }
-}
-btnVideo.onclick = function () {
-  togglePlayPause();
-};
-$('.btn__video').on('click', function () {
-  $(this).closest('top__inner').removeClass('top-play');
-  $(this).parent().addClass('top-play');
-});
 
+// video terasy page
+
+//// scroll
 window.addEventListener('scroll', function () {
   var header = document.querySelector('.header');
   header.classList.toggle('sticky', window.scrollY > 0);
 });
 
+// navbar
 const menuIcon = document.querySelector('.navbar__btn');
 const navbar = document.querySelector('.navbar');
 menuIcon.addEventListener('click', () => {
   navbar.classList.toggle('change');
 });
 
-$('.shops__btn').on('click', function () {
-  $(this).closest('shops__item').removeClass('active');
-  $(this).parent().addClass('active');
+// window.onload = function () {
 
-  return false;
+// };
+
+/// text more
+const btnMore = document.querySelector('.more');
+const shopDetailBox = document.querySelector('.shop-detail__box');
+btnMore.addEventListener('click', () => {
+  shopDetailBox.classList.toggle('vision');
 });
