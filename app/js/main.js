ab875db8d8629b61faf806b97bab66745584b20d
mizza-cz <mizza.cz666@gmail.com>
2021-05-01 19:33:01 +0200

    add fixes

--- app/js/main.js
+++ app/js/main.js
@@ -42,8 +42,8 @@ jQuery(document).ready(function ($) {
   var player;
 
   onYouTubeIframeAPIReady = function () {
-    player = new YT.Player('player', {
-      videoId: 'G__13eEzADk', // youtube video id
+    player = new YT.Player('player iframe', {
+      // videoId: 'G__13eEzADk', // youtube video id
       playerVars: {
         autoplay: 0,
         rel: 0,
