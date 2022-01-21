ed5207b51e9c4e66295a2d37cd09618f5d5bfc68
mizza-cz <mizza.cz666@gmail.com>
2022-01-21 10:38:58 +0100

    fix js

--- app/js/main.js
+++ app/js/main.js
@@ -123,7 +123,7 @@ window.addEventListener('resize', () => {
 //   });
 $(document).ready(function(){
   // Add smooth scrolling to all links
-  $("a").on('click', function(event) {
+  $(".btn-footer").on('click', function(event) {
 
     // Make sure this.hash has a value before overriding default behavior
     if (this.hash !== "") {
